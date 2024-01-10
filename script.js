a71064befe7a21a15534caed92a62f7e2380cc27
Atul Mishra <139470533+atulmishraaaa@users.noreply.github.com>
2024-01-10 16:14:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.querySelector("#myForm").addEventListner("submit",function(event){
+	event.preventDefault();
+
+	const firstName =
+		document.querySelector("#firstName").value;
+	const lastName =
+		document.querySelector("#lastName").value;
+	const number =
+		document.querySelector("#number").value;
+	const email =
+		document.querySelector("#email").value;
+
+	const message = `First Name:${firstName}\nLast Name:${lastName}
+	\nPhone Number:${number}\nEmail ID:${email}`;
+
+	alert(message);
+	
+})
